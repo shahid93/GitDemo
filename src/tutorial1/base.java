b9a3325ba208a3576ba0938b67641fe14e4ce5ae
shahid <shahid.mohammed@glaucuslogistics.com>
2021-05-27 19:54:40 +0530

    first commit

---
+++ src/tutorial1/base.java
@@ -0,0 +1,43 @@
+package tutorial1;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+	public static  AndroidDriver<AndroidElement> capabilities() throws MalformedURLException
+	{
+		
+
+	AndroidDriver<AndroidElement>  driver;
+
+		// TODO Auto-generated method stub
+	 File appDir = new File("src");
+     File app = new File(appDir, "ApiDemos-debug.apk");
+     DesiredCapabilities capabilities = new DesiredCapabilities();
+     
+     capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "2ndemulator"); 
+     capabilities.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
+     capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+     capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+     capabilities.setCapability("chromedriverExecutable","D:\\appium apk\\chromedriver.exe");
+    
+    driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	    
+	 return driver;
+	}
+	
+	public static void main(String[] args) throws MalformedURLException {
+		
+		base b= new base();
+		b.capabilities();
+		
+	}
+}
